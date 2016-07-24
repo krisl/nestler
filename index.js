8291b4f57235945bc309e756f438f40bef673a13
Aaron Lipinski <kris.lipinski@gmail.com>
2016-07-24 16:23:16 +1200

    use vars from outside fn

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-function nestler(get, size, check, checkpos = 0, state = {pos:0}) {
-  return {
-    [Symbol.iterator]() {
-      return {
-        next() {
-          while (state.pos < size) {
-            const item = get(state.pos)
-            state.pos++;
+function nestler(get, size, check) {
+  const state = {pos: 0}
+  const _nestler = function(depth) {
+    return {
+      [Symbol.iterator]() {
+        return {
+          next() {
+            while (state.pos < size) {
+              const item = get(state.pos)
+              state.pos++;
 
-            // check if its at our level
-            if (check[checkpos](item)) {
-              return {
-                value: {
-                  parent: item,
-                  children: nestler(get, size, check, checkpos+1, state)
-                },
-                done: false
+              // check if its at our level
+              if (check[depth](item)) {
+                return {
+                  value: {
+                    parent: item,
+                    children: _nestler(depth+1)
+                  },
+                  done: false
+                }
+              }
+
+              // check if it belongs to a parent
+              for(var ci = 0; ci < depth; ci++) {
+                if (check[ci](item)) {
+                  state.pos--
+                  return { done: true };
+                }
               }
             }
-
-            // check if it belongs to a parent
-            for(var ci = 0; ci < checkpos; ci++) {
-              if (check[ci](item)) {
-                state.pos--
-                return { done: true };
-              }
-            }
+            return { done: true }
           }
-          return { done: true }
         }
       }
     }
   }
+  return _nestler(0) // start at depth 0
 }
 
 module.exports = nestler
