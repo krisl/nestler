6933657c9a601b7629bcd8c0cac711fe63f7973f
Aaron Lipinski <kris.lipinski@gmail.com>
2016-07-24 15:41:36 +1200

    convert to case statements

--- test/test.js
+++ test/test.js
@@ -39,17 +39,23 @@ describe('nestler', () => {
 
   it('should iterate over level 2', () => {
     for(var l1 of result) {
-      if (l1.parent.name === 'a') {
-        expect([...l1.children]).to.eql([
-          {parent: {level: '2', name: 'aa'}, children: {}},
-          {parent: {level: '2', name: 'ab'}, children: {}},
-          {parent: {level: '2', name: 'ac'}, children: {}},
-          {parent: {level: '2', name: 'ad'}, children: {}}
-        ])
-      } else if (l1.parent.name === 'b') {
-        expect([...l1.children]).to.eql([
-          {parent: {level: '2', name: 'ba'}, children: {}}
-        ])
+      switch (l1.parent.name) {
+        case 'a':
+          expect([...l1.children]).to.eql([
+            {parent: {level: '2', name: 'aa'}, children: {}},
+            {parent: {level: '2', name: 'ab'}, children: {}},
+            {parent: {level: '2', name: 'ac'}, children: {}},
+            {parent: {level: '2', name: 'ad'}, children: {}}
+          ])
+          break;
+        case 'b':
+          expect([...l1.children]).to.eql([
+            {parent: {level: '2', name: 'ba'}, children: {}}
+          ])
+          break;
+
+        default:
+          expect(l2.parent.name).to.equal('!@#$')
       }
     }
   })
@@ -57,21 +63,36 @@ describe('nestler', () => {
   it('should iterate over level 3', () => {
     for(var l1 of result) {
       for(var l2 of l1.children) {
-        if (l2.parent.name === 'aa') {
-          expect([...l2.children]).to.eql([
-            {parent: {level: '3', name: 'aaa'}, children: {}}
-          ])
-        } else if (l2.parent.name === 'ab') {
-          expect([...l2.children]).to.eql([
-            {parent: {level: '3', name: 'aba'}, children: {}},
-            {parent: {level: '3', name: 'abb'}, children: {}}
-          ])
-        } else if (l2.parent.name === 'ac') {
-          expect([...l2.children]).to.eql([])
-        } else if (l2.parent.name === 'ad') {
-          expect([...l2.children]).to.eql([
-            {parent: {level: '3', name: 'ada'}, children: {}}
-          ])
+        switch (l2.parent.name) {
+          case 'aa':
+            expect([...l2.children]).to.eql([
+              {parent: {level: '3', name: 'aaa'}, children: {}}
+            ])
+            break;
+
+          case 'ab':
+            expect([...l2.children]).to.eql([
+              {parent: {level: '3', name: 'aba'}, children: {}},
+              {parent: {level: '3', name: 'abb'}, children: {}}
+            ])
+            break;
+
+          case 'ac':
+            expect([...l2.children]).to.eql([])
+            break;
+
+          case 'ad':
+            expect([...l2.children]).to.eql([
+              {parent: {level: '3', name: 'ada'}, children: {}}
+            ])
+            break;
+
+          case 'ba':
+            expect([...l2.children]).to.eql([])
+            break;
+
+          default:
+            expect(l2.parent.name).to.equal('!@#$')
         }
       }
     }
