f6309f58242bff1f281c552f8b3187187ecf80c8
Aaron Lipinski <kris.lipinski@gmail.com>
2016-07-24 14:12:04 +1200

    first cut

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-function nestler() {
+function nestler(list, size, check, checkpos = 0, state = {pos:0}) {
+  return {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          while (state.pos < size) {
+            const item = list[state.pos]
+            state.pos++;
 
+            // check if its at our level
+            if (check[checkpos](item)) {
+              return {
+                value: {
+                  parent: item,
+                  children: nestler(list, size, check, checkpos+1, state)
+                },
+                done: false
+              }
+            }
+
+            // check if it belongs to a parent
+            for(var ci = 0; ci < checkpos; ci++) {
+              if (check[ci](item)) {
+                state.pos--
+                return { done: true };
+              }
+            }
+          }
+          return { done: true }
+        }
+      }
+    }
+  }
 }
 
 module.exports = nestler
